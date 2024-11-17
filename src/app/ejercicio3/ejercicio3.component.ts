a8912ce05a0f1b2076e3dc83059b9a12da086032
CESAR-28 <cesarcaceres9528.cc@gmail.com>
2024-11-17 11:00:53 -0500

    Ejercicio tres resuelto

--- src/app/ejercicio3/ejercicio3.component.ts
+++ src/app/ejercicio3/ejercicio3.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ejercicio3',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './ejercicio3.component.html',
   styleUrl: './ejercicio3.component.css'
 })
 export class Ejercicio3Component {
+
+  calificaciones: number[] = [0, 0, 0];
+  resultado: string = '';
+
+  verificarAprobacion(): void {
+    const promedio = (this.calificaciones[0] + this.calificaciones[1] + this.calificaciones[2]) / 3;
+    this.resultado = promedio >= 70 ? 'El alumno aprueba.' : 'El alumno reprueba.';
+  }
 
 }
